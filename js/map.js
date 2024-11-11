e32874576d1bffb14370610dda3d48056442e5df
nthomas211 <nthomas21@hawk.iit.edu>
2024-11-11 00:17:56 -0500

    added arrow market

--- js/map.js
+++ js/map.js
@@ -1,8 +1,15 @@
-  function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.87365210641366, lng: -87.77835794192839},
-      zoom: 14
-    });
+  function init(){
+  var map = new google.maps.Map(document.getElementById('map'), { center: {lat: 41.88696692015735, lng: -87.78954637418263},
+      zoom: 14 });
+  var lake = new google.maps.LatLng(41.887000, -87.821107);
+  var marker =new google.maps.Marker ({
+   position: {lat: 41.887000, lng: -87.821107},
+   map: map,
+   animation: google.maps.Animation.BOUNCE,
+   icon: 'media/down.png'
+  })
+   
+  
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', init);
